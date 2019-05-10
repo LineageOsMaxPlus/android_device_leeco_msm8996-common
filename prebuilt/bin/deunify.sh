f772631a9462692010b5f52d79c365f8e6f1fcbb
ThE_MarD <themard85@gmail.com>
2019-05-09 23:18:18 -0600

    msm8996-common: commonize NFC
    * max_plus has NFC (PN548)
    * remove NFC if x2 is detected
    
    Change-Id: Ia2ba6c2374616b5ea51625a270c06ede2ead5641

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -17,6 +17,22 @@ case "$DEVINFO" in
     mv -f /vendor/firmware/max_plus/* /vendor/firmware/
     ;;
 
+  le_x2*)
+    # Remove NFC configs
+    rm -f /system/etc/libnfc-nci.conf
+    rm -f /vendor/etc/libnfc-nxp_RF.conf
+    rm -f /vendor/etc/libnfc-nxp.conf
+
+    # Remove NFC perms
+    rm -f /vendor/etc/permissions/android.hardware.nfc*
+    rm -f /vendor/etc/permissions/com.android.nfc*
+    rm -f /vendor/etc/permissions/com.nxp.mifare.xml
+
+    # Remove NFC services
+    rm -f /vendor/etc/init/android.hardware.nfc*
+    rm -f /vendor/etc/init/vendor.nxp.hardware.nfc*
+    ;;
+
   le_zl0*)
     # Move firmware
     mv -f /vendor/firmware/zl0/* /vendor/firmware/
